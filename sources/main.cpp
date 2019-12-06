3acc617344874e001630343f73c1151f568e03f9
CrazyOverdose <alenkavh@yandex.ru>
2019-12-06 10:50:41 +0300

    test 11

--- sources/main.cpp
+++ sources/main.cpp
@@ -4,12 +4,12 @@
 
 //using namespace std;
 
-int main () {
-string a="/home/absinthetoxin/CrazyOverdose/";
+int main (){
+string a = "/home/absinthetoxin/CrazyOverdose/";
 string b = "lab04_boost_filesystem/misc/ftp";
-string path_to_ftp = a+b;
-analysis example (path_to_ftp);
-example.work (path_to_ftp);
+string path_to_ftp = a + b;
+analysis example(path_to_ftp);
+example.work(path_to_ftp);
 example.print_name_files();
 example.print_information();
 }
