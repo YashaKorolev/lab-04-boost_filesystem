d47748e2dc6291e4fcf1df7cf4dce86660188701
CrazyOverdose <alenkavh@yandex.ru>
2019-12-06 15:03:55 +0300

    test 10

--- sources/main.cpp
+++ sources/main.cpp
@@ -3,11 +3,10 @@
 #include <iostream>
 #include "analysis.hpp"
 
-//using namespace std;
-
 int main(){
-analysis example(".../lab04_boost_filesystem/misc/ftp");
-example.work(".../lab04_boost_filesystem/misc/ftp");
+std::string a = "/home/absinthetoxin/CrazyOverdose/lab-04-boost-filesystem/misc/ftp";
+analysis example(a);
+example.work(a);
 example.print_name_files();
 example.print_information();
 }
