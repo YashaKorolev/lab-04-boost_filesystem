090761c90674f1f9e4ed7f11ef5e15775182e918
CrazyOverdose <alenkavh@yandex.ru>
2019-12-06 12:06:50 +0300

    test 29

--- sources/main.cpp
+++ sources/main.cpp
@@ -6,8 +6,8 @@
 //using namespace std;
 
 int main(){
-analysis example("/home/absinthetoxin/CrazyOverdose/lab04_boost_filesystem/misc/ftp");
-example.work("/home/absinthetoxin/CrazyOverdose/lab04_boost_filesystem/misc/ftp");
+analysis example(".../lab04_boost_filesystem/misc/ftp");
+example.work(".../lab04_boost_filesystem/misc/ftp");
 example.print_name_files();
 example.print_information();
 }
